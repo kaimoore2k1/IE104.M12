dc62cb3624481d3986914a11361d43e2afa915ca
Kai Moore <haohao12012001@gmail.com>
2021-11-23 15:03:41 +0700

    link cac page

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function showChatBox() {
-    document.getElementById("showChatBox").style.display = "block";
-}
-
-function hideChatBox() {
-    document.getElementById("showChatBox").style.display = "none";
-}
