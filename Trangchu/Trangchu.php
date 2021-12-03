ed3e087297ae181db6992975d6b4c19b1d7bfcdd
Kai Moore <haohao12012001@gmail.com>
2021-12-03 18:54:48 +0700

    sua font size text 14px

--- Trangchu/Trangchu.php
+++ Trangchu/Trangchu.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Header/Header.css">
     <link rel="stylesheet" href="Trangchu.css">
+    <script src="//code.tidio.co/kejfzg9uqghunwx4afuy5fvtocc7uk97.js" async></script>
     <title>UITour</title>
 </head>
 
--- Trangchu/test.php
+++
@@ -1,2 +0,0 @@
-
-<?php include "../Header/Header.html" ?>
