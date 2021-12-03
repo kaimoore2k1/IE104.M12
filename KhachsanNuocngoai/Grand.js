ba3f45fb44c4b3cc41d86306c9b6bbccee359126
ChienPham231201 <19521286@gm.uit.edu.vn>
2021-12-03 21:10:48 +0700

    done

---
+++ KhachsanNuocngoai/Grand.js
@@ -0,0 +1,36 @@
+const selected = document.querySelectorAll('.wapper__content__nav button')
+const content_selected = document.querySelectorAll('.wapper__content--content')
+
+function chuongtrinh_selected() {
+
+    selected[0].setAttribute('id', 'wapper__content__nav--selected')
+    selected[1].removeAttribute('id', 'wapper__content__nav--selected')
+    selected[2].removeAttribute('id', 'wapper__content__nav--selected')
+
+    content_selected[0].style.display = "block"
+    content_selected[1].style.display = "none"
+    content_selected[2].style.display = "none"
+
+}
+
+function chinhsach_selected() {
+
+    selected[1].setAttribute('id', 'wapper__content__nav--selected')
+    selected[0].removeAttribute('id', 'wapper__content__nav--selected')
+    selected[2].removeAttribute('id', 'wapper__content__nav--selected')
+
+    content_selected[1].style.display = "block"
+    content_selected[0].style.display = "none"
+    content_selected[2].style.display = "none"
+}
+
+function thacmac_selected() {
+
+    selected[2].setAttribute('id', 'wapper__content__nav--selected')
+    selected[1].removeAttribute('id', 'wapper__content__nav--selected')
+    selected[0].removeAttribute('id', 'wapper__content__nav--selected')
+
+    content_selected[2].style.display = "block"
+    content_selected[1].style.display = "none"
+    content_selected[0].style.display = "none"
+}
